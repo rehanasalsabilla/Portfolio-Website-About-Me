4347fbf14ec82d194fbc80e4e4345f912cef898c
Rehana <rehanaputri814@gmail.com>
2026-04-08 20:45:03 +0700

    Skill-1

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,7 +27,7 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="bg-gradient-to-br from-cyan-800 via-gray-900 to-[#162d64] text-white py-20 px-6 md:px-16"
+      className="bg-linear-to-br from-cyan-800 via-gray-900 to-[#162d64] text-white py-20 px-6 md:px-16"
     >
       <div className="max-w-7xl mx-auto">
         
