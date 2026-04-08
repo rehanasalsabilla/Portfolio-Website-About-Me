0727625168fb7f7e9379c80f09deef77336b0159
Rehana <rehanaputri814@gmail.com>
2026-04-08 20:23:51 +0700

    Experience-5

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -55,7 +55,7 @@ const Experience = () => {
       </motion.div>
 
       {/* GRID 2 KOLOM */}
-      <div className="grid md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 max-w-7xl mx-auto">
         {experiences.map((exp, index) => (
           <motion.div
             key={index}
